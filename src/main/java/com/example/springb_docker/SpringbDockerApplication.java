4bf981350b4ff5df01a3a293e0438e6ee5580fd0
Enrique Hdez <soyenriqueh@hotmail.com>
2025-10-30 13:33:24 +0100

    init commit

---
+++ src/main/java/com/example/springb_docker/SpringbDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springb_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbDockerApplication.class, args);
+	}
+
+}
